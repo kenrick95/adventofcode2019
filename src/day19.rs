5c278ba46906b647083d752a74188243b652b1aa
Kenrick <kenrick95@gmail.com>
2019-12-19 14:00:22 +0800

    Day 19 debug

--- src/day19.rs
+++ src/day19.rs
@@ -1,10 +1,12 @@
 use super::program::*;
+use console::Term;
 use std::cell::RefCell;
 use std::collections::HashMap;
 
 pub fn main() {
     let positions = super::utils::get_list_of_numbers_from_file::<i128>("./src/day19-real.log");
     part1(&positions);
+    // part2(&positions);
 }
 
 fn get_result_on_location(raw_positions: &[i128], location: (i128, i128)) -> i128 {
@@ -41,17 +43,39 @@ fn get_result_on_location(raw_positions: &[i128], location: (i128, i128)) -> i12
 }
 
 fn part1(positions: &[i128]) {
-    // let map: RefCell<HashMap<(usize, usize), bool>> = RefCell::new(HashMap::new());
+    let mut map: HashMap<(usize, usize), bool> = HashMap::new();
     let mut answer = 0;
-    for y in 0..50 {
-        for x in 0..50 {
-            answer += get_result_on_location(positions, (y,x));
-            // (*map.borrow_mut()).insert(
-            //     (y as usize, x as usize),
-            //     if result == 1 { true } else { false },
-            // );
+    for y in 0..125 {
+        for x in 0..125 {
+            let result = get_result_on_location(positions, (y, x));
+            if x < 50 && y < 50 {
+                answer += result;
+            }
+            map.insert(
+                (y as usize, x as usize),
+                if result == 1 { true } else { false },
+            );
         }
     }
 
+    print_map(&map);
     println!("Answer part 1: {:?}", answer);
+}
+
+fn print_map(map: &HashMap<(usize, usize), bool>) {
+    let term = Term::stdout();
+    let mut max_x = 0;
+    let mut max_y = 0;
+    term.clear_screen();
+    for (location, &cell) in map.iter() {
+        term.move_cursor_to(location.1, location.0);
+        max_x = std::cmp::max(max_x, location.1);
+        max_y = std::cmp::max(max_y, location.0);
+        term.write_str(if cell { "#" } else { "." });
+    }
+    term.move_cursor_to(max_x, max_y);
+    term.write_line("");
+    term.write_line(&max_y.to_string());
+    term.write_line(&max_x.to_string());
+    // let input = term.read_key().unwrap();
 }
